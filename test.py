f305589162bf644bfe65ab5e94aa6c780b9fa5b4
王耀辉 <w1792102245@163.com>
2022-02-18 19:00:41 +0800

    隐形眼镜

---
+++ test.py
@@ -0,0 +1,9 @@
+# with open("sklearn_data/lenses.data") as f:
+#     def strip_space(l):
+#         print(l)
+#
+#     lenses = [inst.strip().split() for inst in f.readlines()]
+#     print(lenses)
+
+
+print(list(map(int,["a","2"])))
