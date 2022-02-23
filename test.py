0e5d23ca6f96dd5573c45735c8056cfdf6de2f99
王耀辉 <w1792102245@163.com>
2022-02-23 18:05:30 +0800

    逻辑分类实现

--- test.py
+++ test.py
@@ -7,7 +7,7 @@
 
 
 # print(list(map(int,["a","2"])))
-import numpy as np
+# import numpy as np
 # with open("sklearn_data/lenses.data") as f:
 #     lenses = [inst.strip("\n").split("  ") for inst in f.readlines()]
 #     lenses = np.array(lenses)
@@ -19,4 +19,15 @@ import numpy as np
 #
 #     # print(np.where(map(result_set,result_set_label), result_set, result_set_label))
 #     lensesLabels = ["age","prescript","astigmatic","tearRate"]
-print(np.power(3,2))
+# print(np.power(3,2))
+
+
+# #numpy 和 sympy合用
+# import numpy as np
+# import sympy as sp
+# xm = np.array([-np.pi/2,0,np.pi/2])
+#
+# x= sp.symbols("x")
+# z =sp.sin(x)
+# f = sp.lambdify(("x"),z,"numpy")
+# print(f(xm))
